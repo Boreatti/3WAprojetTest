04bdfa9add97e52e26641058643d769a217cc51a
Morgan Boreatti <m.boreatti@gmail.com>
2017-12-05 19:21:37 +0100

    Add files via upload

---
+++ parties/partie.php
@@ -0,0 +1,30 @@
+<?php 
+//chargement du modele
+include('../script/init.php');
+
+
+//Verification de l'id de la partie chargée 
+$erreur='';
+ // on vérifie si on a bien une clef 'id' en GET
+		if(isset($_GET['id'])){
+		    // on vérifie s'assure que l'id ne va pas au dela de 6
+		    if(($_GET['id']) < 6){
+		        // on tente de charger les elements de partie
+				$partId = getPartie('*');
+				$partNum = getPartie('numero');
+				$partTitre = getPartie('titre');
+				$partPropos = getPartie('aPropos');
+				$partDesc = getPartie('description');
+				$partImg = getPartie('image');
+				$listeChpPartie = getChpPartie($_GET['id']); 
+		        
+		    }
+		    else{
+		        $erreur = 'Oups ! Cette partie n existe pas : partie ' . $_GET['id'];
+		    }
+		}else{
+		    $erreur = 'Aucune partie séléctionnée :(';
+		}
+
+
+include('partieTemplate.phtml');
