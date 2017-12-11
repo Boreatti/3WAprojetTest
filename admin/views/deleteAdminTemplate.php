089400e739216828788151aae4c98015c4e7adb6
Morgan Boreatti <m.boreatti@gmail.com>
2017-12-11 16:30:33 +0100

    Add files via upload

--- admin/views/deleteAdminTemplate.php
+++ admin/views/deleteAdminTemplate.php
@@ -2,23 +2,30 @@
 
 <main class="main">
 
-	<h2>Voulez-vous supprimer le <?= $chp['titre'] ?> ? </h2>
+<?php if(!empty($messErreur)): ?>
+<h2><?= $messErreur ?></h2>
+<?php else : ?>
 
-
-		<div>
-			<h3><?= $chp['titre'] ?></h3>
-			<h4>Dans la partie <?= $chp['partie'] ?></h4>
-			<br/>
-			<img src="../parties/img/<?= $chp['image'] ?>" style="float:left; " />
-			<div style="float:right; width:60%;"><?= $chp['html'] ?></div>
+	<?php if(empty($chp)): ?>
+	<h2>Numéro de chapitre incorrect</h2>
+	
+	<?php else : ?>
+		<h2>Voulez-vous supprimer le <?= $chp['titre'] ?> ? </h2>
+			<div>
+				<h3><?= $chp['titre'] ?></h3>
+				<h4>Dans la partie <?= $chp['partie'] ?></h4>
+				<br/>
+				<img src="../parties/img/<?= $chp['image'] ?>" style="float:left; " />
+				<div style="float:right; width:60%;"><?= $chp['html'] ?></div>
+			</div>
+	<div class="clear"></div>
+	<br/>
+		<div class="formStyle">
+			<form method="post">
+				<input type="submit" name="suppr" value="Oui" />
+				<input type="button" name="annuler" onclick="window.location.replace('partie.php?id=<?= $chp["partie"] ?>')" value="Annuler" />
+			</form>
 		</div>
-<div class="clear"></div>
-<br/>
-	<div class="formStyle">
-		<form method="post">
-			<input type="submit" name="suppr" value="Oui" />
-			<input type="button" name="annuler" onclick="window.location.replace('partie.php?id=<?= $chp["partie"] ?>')" value="Annuler" />
-		</form>
-	</div>
-
+	<?php endif ?>
+<?php endif ?>
 </main>
--- admin/views/formulaireTemplate.php
+++ admin/views/formulaireTemplate.php
@@ -8,90 +8,86 @@ include('creationModification.php');
 
 <?php if(!empty($messErreur)): ?>
 <h2><?= $messErreur ?></h2>
+
 <?php else : ?>
-
-<?php if(empty($chp)): ?>
-<h2>Creation</h2>
-<?php else : ?>
-
-
-
-
-
-<h2>Chapitre Acutel</h2>
-<div>
-	<h3><?= $chp['titre'] ?></h3>
-	<h4>Dans la partie <?= $chp['partie'] ?></h4>
+	<?php if(empty($chp)): ?>
+	<h2>Creation</h2>
+	
+	<?php else : ?>
+	<h2>Chapitre Acutel</h2>
+	<div>
+		<h3><?= $chp['titre'] ?></h3>
+		<h4>Dans la partie <?= $chp['partie'] ?></h4>
+		<br/>
+		<img  src="../parties/img/<?= $chp['image'] ?>" />
+		<p><?= $chp['html'] ?></p>
+	</div>
 	<br/>
-	<img  src="../parties/img/<?= $chp['image'] ?>" />
-	<p><?= $chp['html'] ?></p>
-</div>
-<br/>
-<h2>Modification</h2>
-<?php endif ?>
+	<h2>Modification</h2>
+	<?php endif ?>
 
 
-<a class="retourListe" href="index.php">Retour liste</a>
-<?php if(empty($chp)): ?>  
+	<a class="retourListe" href="index.php">Retour liste</a>
+	<?php if(empty($chp)): ?>  
 
 
-<form method="post" enctype="multipart/form-data" action=''>
-	<label for="field_titre">Ajouter une image : </label>
-    <input type="file" name="image"/> 
-    <br/>
+	<form method="post" enctype="multipart/form-data" action=''>
+		<label for="field_titre">Ajouter une image : </label>
+	    <input type="file" name="image"/> 
+	    <br/>
 
 
 
-<?php else : ?>
-<p>Image actuelle : <?= $chp['image'] ?></p>
-<br/>
-<img class="imageActuelle" src="../parties/img/<?= $chp['image'] ?>" />
-<br/>
-<form method="post" enctype="multipart/form-data" action=''>
-	<label for="field_titre">Charger une nouvelle image </label>
-    <input type="file" name="image"/> 
-    <br/>
+	<?php else : ?>
+	<p>Image actuelle : <?= $chp['image'] ?></p>
+	<br/>
+	<img class="imageActuelle" src="../parties/img/<?= $chp['image'] ?>" />
+	<br/>
+	<form method="post" enctype="multipart/form-data" action=''>
+		<label for="field_titre">Charger une nouvelle image </label>
+	    <input type="file" name="image"/> 
+	    <br/>
 
-<?php endif ?>
+	<?php endif ?>
 
-<div class="formStyle">
-	<form method="post">
+	<div class="formStyle">
+		<form method="post">
 
-		<label for="field_titre"><br/>Partie : </label>
-		<select name='partie'><?php foreach($allParties as $key => $partie):?> 
-			<option value="<?= $partie['numero'] ?>" <?php if (!empty($chp) && ($chp['partie'] == $partie['numero'])){echo "selected='selected'";} ?>><?= $partie['numero'] ?> - <?= $partie['titre'] ?></option>
-			<?php endforeach ?>
-		</select>
-		<br/>
+			<label for="field_titre"><br/>Partie : </label>
+			<select name='partie'><?php foreach($allParties as $key => $partie):?> 
+				<option value="<?= $partie['numero'] ?>" <?php if (!empty($chp) && ($chp['partie'] == $partie['numero'])){echo "selected='selected'";} ?>><?= $partie['numero'] ?> - <?= $partie['titre'] ?></option>
+				<?php endforeach ?>
+			</select>
+			<br/>
 
-		<label for="field_titre"><br/>Titre : </label>
-		<input id="field_titre" type='text' name='titre' size='30' maxlength="255" value="<?= (empty($chp)) ? '' : $chp['titre'] ?>"/>
-		<br/>
+			<label for="field_titre"><br/>Titre : </label>
+			<input id="field_titre" type='text' name='titre' size='30' maxlength="255" value="<?= (empty($chp)) ? '' : $chp['titre'] ?>"/>
+			<br/>
 
-		<label for="field_texte">HTML : </label>
-		<textarea id="field_texte" type='text' name='html' size='225' maxlength="255" value=""/><?= (empty($chp['html'])) ? '' : $chp['html'] ?></textarea>
-		<br/>
+			<label for="field_texte">HTML : </label>
+			<textarea id="field_texte" type='text' name='html' size='225' maxlength="255" value=""/><?= (empty($chp['html'])) ? '' : $chp['html'] ?></textarea>
+			<br/>
 
-		<label for="field_texte">CSS : </label>
-		<textarea id="field_texte" type='text' name='css' size='225' maxlength="255" value=""/><?= (empty($chp['css'])) ? '' : $chp['css'] ?></textarea>
-		<br/>
+			<label for="field_texte">CSS : </label>
+			<textarea id="field_texte" type='text' name='css' size='225' maxlength="255" value=""/><?= (empty($chp['css'])) ? '' : $chp['css'] ?></textarea>
+			<br/>
 
 
-		<input type="hidden" name='id' value="<?= (empty($chp)) ? ' ' : $chp['id']?>"/>
-		
+			<input type="hidden" name='id' value="<?= (empty($chp)) ? ' ' : $chp['id']?>"/>
+			
 
-		<?php if(empty($chp)): ?>
-		<input id="field_title" type="submit" value="Ajouter"/>
-		<input type="button" name="annuler" value="Annuler" />
-		<?php else: ?>
-		<input id="field_title" type="submit" value="Modifier"/>
-		<!-- <input type="button" name="annuler" value="Annuler" /> -->
-		<input type="button" name="annuler" onclick="window.location.replace('partie.php?id=<?= $chp['partie'] ?>')" value="Annuler" />
-		<?php endif ?>
+			<?php if(empty($chp)): ?>
+			<input id="field_title" type="submit" value="Ajouter"/>
+			<input type="button" name="annuler" value="Annuler" />
+			<?php else: ?>
+			<input id="field_title" type="submit" value="Modifier"/>
+			<!-- <input type="button" name="annuler" value="Annuler" /> -->
+			<input type="button" name="annuler" onclick="window.location.replace('partie.php?id=<?= $chp['partie'] ?>')" value="Annuler" />
+			<?php endif ?>
+		</form>
+	</div>
+
 	</form>
-</div>
-
-</form>
 <?php endif ?>
 
 
