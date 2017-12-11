85ea51f8675ccdf1eb62c4420223ba2176178a06
Morgan Boreatti <m.boreatti@gmail.com>
2017-12-11 15:33:56 +0100

    Update config.exemple.php

--- script/config.exemple.php
+++ script/config.exemple.php
@@ -5,4 +5,4 @@ $config['dbname'] = '*******';
 $config['dbhost'] = '*******';
 $config['dbuser'] = '*******';
 $config['dbpassword'] = '*******';
-
+$config['adminpassword'] = '$2y$10$M6mLkXrur4sbVBLRpUJZf.Ac5FcK./sINq0EsDj.g851bmTu2YH/y'; //chattons
