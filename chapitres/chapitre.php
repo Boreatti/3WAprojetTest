c13ba012a4073d1be821f17aed3379402d8ea0c3
Morgan Boreatti <m.boreatti@gmail.com>
2017-12-13 16:38:36 +0100

    Add files via upload

--- chapitres/chapitre.php
+++ chapitres/chapitre.php
@@ -6,11 +6,7 @@ $listeChp = getAllChp();
 
 
 if(isset($_GET['id']) && is_numeric($_GET['id'])){
-	$chpId = getChp('*');
-	$chpTitre = getChp('titre');
-	$chpImage = getChp('image');
-	$chpHtml = getChp('html');
-	$chpCss = getChp('css');
+	$chp = getChp('*');
 
 }
 else{
