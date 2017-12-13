04655081026b1fca39d800b56c73d16b7bc49e05
Morgan Boreatti <m.boreatti@gmail.com>
2017-12-13 18:06:03 +0100

    Add files via upload

--- admin/formulaire.php
+++ admin/formulaire.php
@@ -4,12 +4,12 @@ include('initAdmin.php');
 //Verification de l'id de la partie chargée 
 $erreur='';
  // on vérifie si on a bien une clef 'id' en GET
-		if(isset($_GET['id'])){
+		if(isset($_GET['id']) && is_numeric($_GET['id'])){
 		    // on vérifie s'assure que l'id ne va pas au dela de 6
 		    if(is_numeric($_GET['id'])){
 		        // on tente de charger les elements du chapitre
 				$chpId = getChp($_GET['id']);
-				$chp = getChp('*');      
+				$chp = getChp('*');    
 		    }
 		    else{
 		        $erreur = 'Oups ! Ce chapitre n existe pas : chapitre ' . $_GET['id'];
--- admin/partie.php
+++ admin/partie.php
@@ -5,7 +5,7 @@ include('initAdmin.php');
 //Verification de l'id de la partie chargée 
 $erreur='';
  // on vérifie si on a bien une clef 'id' en GET
-		if(isset($_GET['id'])){
+		if(isset($_GET['id']) && is_numeric($_GET['id'])){
 		    // on vérifie s'assure que l'id ne va pas au dela de 6
 		    if(($_GET['id']) < 6){
 		        // on tente de charger les elements de partie
@@ -14,7 +14,7 @@ $erreur='';
 		        
 		    }
 		    else{
-		        $erreur = 'Oups ! Cette partie n existe pas : partie ' . $_GET['id'];
+		        $erreur = "Oups ! Cette partie n'existe pas : partie " . $_GET['id'];
 		    }
 		}else{
 		    $erreur = 'Aucune partie séléctionnée :(';
