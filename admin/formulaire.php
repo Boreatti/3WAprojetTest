b8483db1b3b105def8a381b3ff2583e9c1ae85b1
Morgan Boreatti <m.boreatti@gmail.com>
2017-12-13 19:55:26 +0100

    Add files via upload

--- admin/formulaire.php
+++ admin/formulaire.php
@@ -5,19 +5,36 @@ include('initAdmin.php');
 $erreur='';
  // on vérifie si on a bien une clef 'id' en GET
 		if(isset($_GET['id']) && is_numeric($_GET['id'])){
-		    // on vérifie s'assure que l'id ne va pas au dela de 6
-		    if(is_numeric($_GET['id'])){
-		        // on tente de charger les elements du chapitre
-				$chpId = getChp($_GET['id']);
-				$chp = getChp('*');    
-		    }
-		    else{
-		        $erreur = 'Oups ! Ce chapitre n existe pas : chapitre ' . $_GET['id'];
-		    }
-		}else{
+			$arrayChp = getAllIdChp();
+			$id = $_GET['id'];
+			$chpId = getChp($_GET['id']);
+
+			foreach ($arrayChp as $key => $values) {
+				// var_dump($values);
+			    // on verifie si le GET correspond à une clé dans le tableau des chapitres
+			    if($values['id'] == $id){
+			        // on tente de charger les elements du chapitre
+					$chp = getChp('*');   
+					break; 
+			    }
+			}
+
+			if(empty($chpId)){
+			        $erreur = "Oups ! Ce chapitre n'existe pas : chapitre " . $_GET['id'];
+			    }
+		}
+		else{
 		    $erreur = 'Aucun chapitre séléctionné :(';
 		}
 
 $allParties = getAllParties();
 
+
+// if(isset($_GET['id']) && is_numeric($_GET['id'])){
+// 	$chp = getChp('*');
+
+// }
+
+
 include('views/formulaireTemplate.php'); 
+
